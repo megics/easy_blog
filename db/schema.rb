b6b0ac5e7c153349688b35c0da946f450e019330
melihagizemcelik <meliha.gizem@e-turkiye.gen.tr>
2017-08-21 15:19:56 +0300

    comment modeli oluşturuldu & comment.rb düzenlendi.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170820182020) do
+ActiveRecord::Schema.define(version: 20170821120912) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "profiles", force: :cascade do |t|
