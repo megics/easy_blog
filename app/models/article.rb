9bb0c93a66954a9b722d5da7e7df362759605dfd
melihagizemcelik <meliha.gizem@e-turkiye.gen.tr>
2017-08-22 14:58:09 +0300

    Article'lara category eklendi

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 3 }
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_and_belongs_to_many :categories
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170821131309) do
+ActiveRecord::Schema.define(version: 20170822115655) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,18 @@ ActiveRecord::Schema.define(version: 20170821131309) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "articles_categories", id: false, force: :cascade do |t|
+    t.integer "category_id", null: false
+    t.integer "article_id", null: false
+    t.index ["category_id", "article_id"], name: "index_articles_categories_on_category_id_and_article_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "comments", force: :cascade do |t|
