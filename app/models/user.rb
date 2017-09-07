3df7ae85fc4d6b22bb2bb6d688050e5ec7051dfd
melihagizemcelik <meliha.gizem@e-turkiye.gen.tr>
2017-09-07 14:55:50 +0300

    görünüm düzenlemeleri yapıldı

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/avatar.png"
   #validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   attr_accessor :avatar_file_name
+  enum gender: [ :female, :male ]
 
 end
