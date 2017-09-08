9472e2b19b93596d2f262424d347f61a82e8b7a9
melihagizemcelik <meliha.gizem@e-turkiye.gen.tr>
2017-09-08 11:09:06 +0300

    .idea silindi

--- db/migrate/20170822115655_create_join_table_category_article.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableCategoryArticle < ActiveRecord::Migration[5.1]
-  def change
-    create_join_table :categories, :articles do |t|
-       t.index [:category_id, :article_id]
-      # t.index [:article_id, :category_id]
-    end
-  end
-end
