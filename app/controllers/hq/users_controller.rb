afbe8b6a4e2bdda38b34ba7babfcad7d9a627466
melihagizemcelik <meliha.gizem@e-turkiye.gen.tr>
2017-09-15 12:06:18 +0300

    edit actions in hq/users/:id page

--- app/controllers/hq/users_controller.rb
+++ app/controllers/hq/users_controller.rb
@@ -9,7 +9,7 @@ class Hq::UsersController < Hq::ApplicationController
   end
 
   def show
-    add_breadcrumb @user.name, hq_users_path(@user)
+    add_breadcrumb @user.email, hq_users_path(@user)
     respond_with(@user)
   end
 
