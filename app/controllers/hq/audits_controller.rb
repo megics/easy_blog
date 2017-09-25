e17d2fff96c028d7be799c899c6e625d3f3dd7ea
Meliha Gizem Çelik <meliha.gizem@e-turkiye.gen.tr>
2017-09-25 12:14:44 +0300

    edit controller

--- app/controllers/hq/audits_controller.rb
+++ app/controllers/hq/audits_controller.rb
@@ -2,10 +2,14 @@ class Hq::AuditsController < ApplicationController
   add_breadcrumb "İşlem Geçmişleri", :hq_audits_path
 
   def index
-
+    @search = User.ransack(params[:q])
+    @audits = @search.result(distinct: true)
+    respond_with(@audits)
   end
 
   def show
+    @audit = Audit.find(params[:id])
+    add_breadcrumb @audit.id hq_audit_path(id: @audit)
   end
 
 end
